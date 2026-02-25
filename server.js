df1e2e31bbb7a99d8317feebde43345e805b0564
mehdi el-hajjame <el-hajjame@outlook.com>
2026-02-25 09:54:36 +0000

    creating the server and the files project

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express();
+
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+  res.status(200)
+    .json({
+      status: 'success',
+    })
+});
+
+app.listen(port, () => {
+  console.log(`app running on port${port}...`);
+});
